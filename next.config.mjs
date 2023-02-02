07cdb01d798195d5fc57b69241d422940d97ad88
Atila Fassina <atila@fassina.eu>
2023-02-02 05:02:09 -0300

    put back production redirect for `docs.xata.io`

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   // basePath: '/docs',
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/docs',
-  //       destination: '/docs/overview',
-  //       permanent: false,
-  //     },
-  //   ]
-  // },
+  async redirects() {
+    return process.env.VERCEL_ENV === 'production'
+      ? [
+          {
+            source: '/:path((?!screenshots|icons|img|sounds|fonts|api).*)*',
+            destination: 'https://xata.io/docs/:path*',
+            permanent: true,
+          },
+        ]
+      : []
+    // {
+    //   source: '/docs',
+    //   destination: '/docs/overview',
+    //   permanent: false,
+    // },
+  },
 }
 
 export default nextConfig
